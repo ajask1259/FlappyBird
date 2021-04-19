0b3185b5c3f93b834714407ec61c9f609ee4c281
ajask1259 <taiko.vita@gmail.com>
2021-04-19 18:10:25 +0900

    課題完成

--- FlappyBird/ViewController.swift
+++ FlappyBird/ViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //SKViewに型を変換
+        let skView = self.view as! SKView
+        
+        //fps表示
+        skView.showsFPS=true
+        
+        //ノード数表示
+        skView.showsNodeCount=true
+        
+        //ビューと同じサイズでシーン作成
+        let scene = GameScene(size: skView.frame.size)
+        
+        //ビューにシーンを表示
+        skView.presentScene(scene)
+        
+    }
+    
+    //ステータスバーを消す
+    override var prefersPointerLocked: Bool{
+        get{
+            return true
+        }
     }
 
 
